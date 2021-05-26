d1147c2df1f88d59695ba56be6bdd461bf8a0495
Marek Cieślar <marek.cieslar@gmail.com>
2021-05-26 12:30:04 +0200

    cors fix

--- functions_dev/todo/helpers.ts
+++ functions_dev/todo/helpers.ts
@@ -1,4 +1,6 @@
 export const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
 };
