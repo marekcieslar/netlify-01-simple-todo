51e57d3616c396b54427f5319ab18b0fe2926928
Marek Cieślar <marek.cieslar@gmail.com>
2021-05-26 13:05:28 +0200

    OPTION

--- functions_dev/todo/helpers.ts
+++ functions_dev/todo/helpers.ts
@@ -2,5 +2,5 @@ export const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTION',
 };
